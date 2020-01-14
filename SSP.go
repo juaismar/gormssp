8399143cc35c491d6ce1d19abd989cbda58dcefa
Juan Iscar <juan.iscar@exponentiateam.com>
2020-01-14 19:37:11 +0100

    Signed-off-by: Juan Iscar <juan.iscar@exponentiateam.com>

--- SSP.go
+++ SSP.go
@@ -128,12 +128,14 @@ func dataOutput(columns map[int]Data, rows *sql.Rows) []interface{} {
 				dt = strconv.Itoa(column.Dt.(int))
 			}
 
-			db := column.Db
-			// Is there a formatter?
-			if column.Formatter != nil {
-				row[dt] = column.Formatter(fields[db], fields)
-			} else {
-				row[dt] = fields[db]
+			if dt != nil{
+				db := column.Db
+				// Is there a formatter?
+				if column.Formatter != nil {
+					row[dt] = column.Formatter(fields[db], fields)
+				} else {
+					row[dt] = fields[db]
+				}
 			}
 
 		}
