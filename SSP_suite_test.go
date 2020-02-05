2f6684b8c1e56a749efdfd6bab987a227a7ebf1a
Juan Iscar <juaismar@inf.upv.es>
2020-02-06 00:26:15 +0100

    creating coverage reports Signed-off-by: Juan Iscar <juaismar@inf.upv.es>

--- SSP_suite_test.go
+++ SSP_suite_test.go
@@ -10,6 +10,4 @@ import (
 func TestSSP(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "SSP Suite")
-	junitReporter := reporters.NewJUnitReporter(os.Getenv("CI_REPORT"))
-    RunSpecsWithDefaultAndCustomReporters(t, "Awesome Suite", []Reporter{junitReporter})
-	
+}
