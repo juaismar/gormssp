7820e182a63fd91b8aa918d9c2dedb5d8c0ca735
Juan Iscar <juan.iscar@exponentiateam.com>
2020-11-19 20:27:51 +0100

    UUID tested Signed-off-by: Juan Iscar <juan.iscar@exponentiateam.com>

--- test/models/user.go
+++ test/models/user.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/jinzhu/gorm"
+	uuid "github.com/satori/go.uuid"
 )
 
 var layoutISO = "2006-01-02"
@@ -11,6 +12,7 @@ var layoutISO = "2006-01-02"
 type User struct {
 	gorm.Model
 
+	UUID       uuid.UUID
 	Name       string
 	Instrument string `gorm:"type:varchar(16)"`
 	Age        uint
@@ -24,8 +26,17 @@ type User struct {
 func GetDefaultData() []User {
 
 	date, _ := time.Parse(layoutISO, "2011-11-11")
+
+	uuidJuan, _ := uuid.FromString("bfe44cb2-c65c-4f37-9672-8437b6718d70")
+	uuidJuAn, _ := uuid.FromString("c14be350-6671-4ffe-8108-608ebcccf036")
+	uuidJoaquin, _ := uuid.FromString("66d13290-ef29-47f8-a291-5bb6474bcc78")
+	uuidEzequiel, _ := uuid.FromString("d7ee5bc2-d112-424c-b213-f3d4bc5989ef")
+	uuidMarta, _ := uuid.FromString("d1adfebc-8048-4db0-9b9b-c03f3eb5a9d4")
+	uuidLaura, _ := uuid.FromString("e4e1f721-c13e-4b7e-a711-887f31570a74")
+
 	return []User{
 		{
+			UUID:       uuidJuan,
 			Name:       "Juan",
 			Instrument: "Tambor",
 			Age:        10,
@@ -36,6 +47,7 @@ func GetDefaultData() []User {
 			Bitcoins:   3.0,
 		},
 		{
+			UUID:       uuidJuAn,
 			Name:       "JuAn",
 			Instrument: "Trompeta",
 			Age:        15,
@@ -46,6 +58,7 @@ func GetDefaultData() []User {
 			Bitcoins:   4.3,
 		},
 		{
+			UUID:       uuidJoaquin,
 			Name:       "Joaquin",
 			Instrument: "Flauta",
 			Age:        18,
@@ -56,6 +69,7 @@ func GetDefaultData() []User {
 			Bitcoins:   7.18,
 		},
 		{
+			UUID:       uuidEzequiel,
 			Name:       "Ezequiel",
 			Instrument: "Trompeta",
 			Age:        13,
@@ -66,6 +80,7 @@ func GetDefaultData() []User {
 			Bitcoins:   82.14,
 		},
 		{
+			UUID:       uuidMarta,
 			Name:       "Marta",
 			Instrument: "Tambor",
 			Age:        15,
@@ -76,6 +91,7 @@ func GetDefaultData() []User {
 			Bitcoins:   3.0,
 		},
 		{
+			UUID:       uuidLaura,
 			Name:       "Laura",
 			Instrument: "Flauta",
 			Age:        10,
