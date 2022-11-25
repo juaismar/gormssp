8c9604bd0d58e496bc5ae2fa68a6777ef74a4ec5
Juan Iscar <juan.iscar@exponentiateam.com>
2022-11-25 15:31:02 +0100

    admit spaces Signed-off-by: Juan Iscar <juan.iscar@exponentiateam.com>

--- test/models/user.go
+++ test/models/user.go
@@ -13,18 +13,19 @@ var layoutISO = "2006-01-02"
 type User struct {
 	gorm.Model
 
-	UUID       uuid.UUID
-	Name       string
-	Instrument string `gorm:"type:varchar(16)"`
-	Age        uint
-	Candies    int
-	BirthDate  time.Time
-	Fun        bool
-	Money      float32
-	Bitcoins   float64
-	Toys       int64
-	End        int
-	Secret     []byte
+	UUID         uuid.UUID
+	Name         string
+	Instrument   string `gorm:"type:varchar(16)"`
+	Age          uint
+	Candies      int
+	BirthDate    time.Time
+	Fun          bool
+	Money        float32
+	Bitcoins     float64
+	Toys         int64
+	End          int
+	Secret       []byte
+	FavoriteSong string `gorm:"column:Favorite song"`
 }
 
 // GetDefaultUser returns data to populate table
@@ -41,82 +42,88 @@ func GetDefaultUser() []User {
 
 	return []User{
 		{
-			UUID:       uuidJuan,
-			Name:       "Juan",
-			Instrument: "Tambor",
-			Age:        10,
-			Candies:    0,
-			BirthDate:  date,
-			Fun:        true,
-			Money:      2.0,
-			Bitcoins:   3.0,
-			Toys:       0,
-			End:        0,
+			UUID:         uuidJuan,
+			Name:         "Juan",
+			Instrument:   "Tambor",
+			Age:          10,
+			Candies:      0,
+			BirthDate:    date,
+			Fun:          true,
+			Money:        2.0,
+			Bitcoins:     3.0,
+			Toys:         0,
+			End:          0,
+			FavoriteSong: "Happy birthday",
 		},
 		{
-			UUID:       uuidJuAn,
-			Name:       "JuAn",
-			Instrument: "Trompeta",
-			Age:        15,
-			Candies:    -10,
-			BirthDate:  date,
-			Fun:        false,
-			Money:      3.1,
-			Bitcoins:   4.3,
-			Toys:       1,
-			End:        1,
+			UUID:         uuidJuAn,
+			Name:         "JuAn",
+			Instrument:   "Trompeta",
+			Age:          15,
+			Candies:      -10,
+			BirthDate:    date,
+			Fun:          false,
+			Money:        3.1,
+			Bitcoins:     4.3,
+			Toys:         1,
+			End:          1,
+			FavoriteSong: "Himno Español",
 		},
 		{
-			UUID:       uuidJoaquin,
-			Name:       "Joaquin",
-			Instrument: "Flauta",
-			Age:        18,
-			Candies:    10,
-			BirthDate:  date,
-			Fun:        true,
-			Money:      3.4,
-			Bitcoins:   7.18,
-			Toys:       2,
-			End:        2,
+			UUID:         uuidJoaquin,
+			Name:         "Joaquin",
+			Instrument:   "Flauta",
+			Age:          18,
+			Candies:      10,
+			BirthDate:    date,
+			Fun:          true,
+			Money:        3.4,
+			Bitcoins:     7.18,
+			Toys:         2,
+			End:          2,
+			FavoriteSong: "гимн России",
 		},
 		{
-			UUID:       uuidEzequiel,
-			Name:       "Ezequiel",
-			Instrument: "Trompeta",
-			Age:        13,
-			Candies:    5,
-			BirthDate:  date,
-			Fun:        false,
-			Money:      22.11,
-			Bitcoins:   82.14,
-			Toys:       2,
-			End:        3,
+			UUID:         uuidEzequiel,
+			Name:         "Ezequiel",
+			Instrument:   "Trompeta",
+			Age:          13,
+			Candies:      5,
+			BirthDate:    date,
+			Fun:          false,
+			Money:        22.11,
+			Bitcoins:     82.14,
+			Toys:         2,
+			End:          3,
+			FavoriteSong: "Viejoven",
 		},
 		{
-			UUID:       uuidMarta,
-			Name:       "Marta",
-			Instrument: "Tambor",
-			Age:        15,
-			Candies:    20,
-			BirthDate:  date,
-			Fun:        false,
-			Money:      2.0,
-			Bitcoins:   3.0,
-			Toys:       1,
-			End:        4,
+			UUID:         uuidMarta,
+			Name:         "Marta",
+			Instrument:   "Tambor",
+			Age:          15,
+			Candies:      20,
+			BirthDate:    date,
+			Fun:          false,
+			Money:        2.0,
+			Bitcoins:     3.0,
+			Toys:         1,
+			End:          4,
+			FavoriteSong: "El chiki chiki",
 		},
 		{
-			UUID:       uuidLaura,
-			Name:       "Laura",
-			Instrument: "Flauta",
-			Age:        10,
-			Candies:    110,
-			BirthDate:  date,
-			Fun:        true,
-			Money:      0.1,
-			Bitcoins:   22.71,
-			Toys:       3,
-			End:        5,
+			UUID:         uuidLaura,
+			Name:         "Laura",
+			Instrument:   "Flauta",
+			Age:          10,
+			Candies:      110,
+			BirthDate:    date,
+			Fun:          true,
+			Money:        0.1,
+			Bitcoins:     22.71,
+			Toys:         3,
+			End:          5,
+			FavoriteSong: "Street Fighter",
 		},
 	}
 }
